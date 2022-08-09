c3c13ad6f0a7f415a750237118a30ceda0bb122c
FockRock <smirnov_nikita@mail.ru>
2022-08-10 00:08:12 +0300

    Homework 2 complete!

--- m2_layout/app/src/main/java/com/nick/m2_layout/MainActivity.kt
+++ m2_layout/app/src/main/java/com/nick/m2_layout/MainActivity.kt
@@ -2,10 +2,17 @@ package com.nick.m2_layout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.nick.m2_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.text.text = "Верхняя строчка, настроенная из кода"
+        binding.text2.text = "Нижняя строчка, настроенная из кода"
     }
 }
