8543c356d0c8f40607a9356714697f492186e0ae
FockRock <smirnov_nikita@mail.ru>
2024-04-09 21:38:45 +0300

    Logic is added to buttons

--- m1_hello_world/app/src/main/java/com/nick/counter/MainActivity.kt
+++ m1_hello_world/app/src/main/java/com/nick/counter/MainActivity.kt
@@ -7,12 +7,24 @@ import com.nick.counter.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private var count = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bMinus.setOnClickListener {
+            if (count != 0) {
+                count--
+                binding.tvNumber.text = count.toString()
+            }
+        }
 
+        binding.bPlus.setOnClickListener {
+            if (count < 50) {
+                count++
+                binding.tvNumber.text = count.toString()
+            }
+        }
     }
 }
