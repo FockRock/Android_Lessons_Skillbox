f1ccd864d0eb173a4328ffc7e7e6b3e4939a6b27
FockRock <smirnov_nikita@mail.ru>
2024-04-17 23:36:24 +0300

    ScrollView and binding added

--- m2_layout/app/src/main/java/com/example/m2_layout/MainActivity.kt
+++ m2_layout/app/src/main/java/com/example/m2_layout/MainActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.m2_layout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    lateinit var bindingClass : ActivityMainBinding
+    override fun onCreate(savedInstanceState:Bundle?){
+        super.onCreate (savedInstanceState)
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView (bindingClass.root)
+
+        bindingClass.s.text = "Lesson 2 started!"
     }
+
 }
