b092b5b871c69963e3541f3a762f3546827b981d
FockRock <smirnov_nikita@mail.ru>
2024-05-01 00:17:38 +0300

    Binding added

--- m3_components/app/src/main/java/com/example/m4_components/MainActivity.kt
+++ m3_components/app/src/main/java/com/example/m4_components/MainActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.m4_components.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var bindingClass : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        bindingClass = ActivityMainBinding.inflate(layoutInflater)
+        setContentView (bindingClass.root)
+
     }
 }
